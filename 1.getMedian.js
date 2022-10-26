54e680672219788a935d535bc18ceb337fecf1aa
École du Web <ledesignerduweb@outlook.fr>
2022-10-26 10:45:20 +0200

    first commit

---
+++ 1.getMedian.js
@@ -0,0 +1,10 @@
+function getMedian(array) {
+
+}
+
+console.log(getMedian([1,2,3,4,5]))                       // 3
+console.log(getMedian([1,2,3,4]))                         // 2.5
+console.log(getMedian([10,15,14,12,16,14,17,5,4,7]));     // 13
+console.log(getMedian([1000,1700,850,1200,1400,1000,900,1500,1000000])); 
+// Médiane   1200
+// Moyenne    112172
